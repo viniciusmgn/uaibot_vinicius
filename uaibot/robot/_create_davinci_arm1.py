3906d5f94b5d04d101c03f422aed356319f9830c
fbartelt <fbarteltp@gmail.com>
2022-08-20 17:58:37 -0300

    Cleanup

--- uaibot/robot/_create_davinci_arm1.py
+++ uaibot/robot/_create_davinci_arm1.py
@@ -21,83 +21,63 @@ def _create_davinci_arm1(color, opacity):
     if (not Utils.is_a_number(opacity)) or opacity < 0 or opacity > 1:
         raise Exception(
             "The parameter 'opacity' should be a float between 0 and 1.")
-    r1 = 1.875  # 0
-    r2 = 3.25
-    r3 = 75.25  # -90.75
-    r4 = 28.75 + 90 #+ 180
-    r5 = -45
-    r6 = 94 + 180
-    r7 = -170.5
-    r8 = 61.5
-    r9 = -15*0
 
-    theta1 = np.deg2rad(0)
-    theta2 = np.deg2rad(r1)  # np.deg2rad(0)
-    theta3 = np.deg2rad(r2 - r1)  # np.deg2rad(5)
-    theta4 = np.deg2rad(r3)
-    theta5 = np.deg2rad(r4)  # -np.pi/2
-    theta6 = np.deg2rad(r5)
-    theta7 = np.deg2rad(r6)
-    theta8 = np.deg2rad(r7)
-    theta9 = np.deg2rad(r8)
-    theta10 = np.deg2rad(r9)
+    theta2 = np.deg2rad(1.875)
+    theta3 = np.deg2rad(3.25 - 1.875)
+    theta4 = np.deg2rad(75.25)
+    theta5 = np.deg2rad(28.75 + 90)
+    theta6 = np.deg2rad(-45)
+    theta7 = np.deg2rad(94 + 180)
+    theta8 = np.deg2rad(-170.5)
+    theta9 = np.deg2rad(61.5)
 
-    d1 = 0
-    d3 = -96e-3  # 0
-    d5 = -96e-3 * 1.9  # -96e-3 * 2.9
-    d6 = (431.8 * 1.417)/1000  # -(144.54 - 431.8)
-    d7 = 0
+    d3 = -96e-3
+    d5 = -96e-3 * 1.9
+    d6 = (431.8 * 1.417)/1000
     d9 = -3.75e-2
-    d10 = 3e-2  # old d6
+    d10 = 3e-2
 
-    alpha1 = 0
-    alpha5 = np.pi*(1/2 + 1/9)# np.pi/2
+    alpha5 = np.pi*(1/2 + 1/9)
     alpha6 = np.pi/2
-    alpha7 = 0
-    alpha8 = 0
     alpha9 = np.pi/2
     alpha10 = np.pi/2*0
 
-    a1 = 0
-    a2 = 0.3#-0.3
-    a3 = 0.415#-0.415  # -0.35
-    a4 = 0.415#-0.415  # -0.407
-    a6 = 3.1e-3#-3.1e-3  # 40.09/1000
-    a7 = 0.27#0.27#0.188
-    a8 = 0.474#0.474
-    a9 = 0.098#0.098
-    a10 = 0.235*0
+    a2 = 0.3
+    a3 = 0.415
+    a4 = 0.415
+    a6 = 3.1e-3
+    a7 = 0.27
+    a8 = 0.474
+    a9 = 0.098
 
     # Passive joints 1, 2, 3, 4, 5, 8, 9
-    # Active joints 6, 7, 10 
+    # Active joints 6, 7, 10
     link_info = np.array([
         # "theta" rotation in z
-        # -> changed [0, 3] from -pi/2 to pi/2
-        [0, 0,   0,  0,      0,      0,      0,      0,      0,      0],
-        #0,  "d" translation in z
-        [d1, 0,  d3,  0,     d5,     d6,     d7,      0,      d9,      d10],
-        #0,  "alfa" rotation in x
-        [alpha1, 0,   0,  0, alpha5, alpha6, alpha7, alpha8, alpha9, alpha10],
-        #0,  "a" translation in x
-        [a1, a2, a3, a4,      0,     a6,     a7,      a8,      a9,      a10],
-        #0,  joint type
-        [1, 0,   0,  0,      0,      0,      0,      0,      0,      1]
+        [0, 0,   0,  0,      0,      0,  0,  0,      0,   0],
+        # "d" translation in z
+        [0, 0,  d3,  0,     d5,     d6,  0,  0,     d9, d10],
+        # "alfa" rotation in x
+        [0, 0,   0,  0, alpha5, alpha6,  0,  0, alpha9,   0],
+        # "a" translation in x
+        [0, a2, a3, a4,      0,     a6, a7, a8,     a9,   0],
+        # joint type
+        [1, 0,   0,  0,      0,      0,  0,  0,      0,   1]
     ])
 
     scale = 1
     n = link_info.shape[1]
     base_3d_obj = []
     link_3d_obj = []
-    mesh = MeshMaterial(metalness=0.5, clearcoat=0, roughness=0.5,
-                        normal_scale=[0.5, 0.5], color=color,
-                        opacity=opacity, side="DoubleSide")
-    # original model is rotated (Robot fron = plane X x Y)
-    q_ = np.array([1, 0, 0, 0, 0, 0, 0]) * -np.pi/2
+    mesh = MeshMaterial(metalness=0.5, clearcoat=0, roughness=0.5, normal_scale=[
+                        0.5, 0.5], color=color, opacity=opacity, side="DoubleSide")
     b1 = 0.1
     b2 = 0.45
     b3 = 1.1
- # * Utils.trn([-0.1, -0.45, -1])
-    Q01 = Utils.trn([b1, b3, -b2]) * Utils.rotx(q_[0]) * Utils.rotz(np.pi) * Utils.rotz(link_info[0, 0] + theta1) * Utils.trn([0, 0, link_info[1, 0]]) * Utils.rotx(link_info[2, 0]) * Utils.trn(
+
+    Q00 = Utils.trn([b1, b3, -b2]) * Utils.rotx(-np.pi/2) * \
+        Utils.rotz(np.pi)  # change reference frame
+    Q01 = Q00 * Utils.rotz(link_info[0, 0]) * Utils.trn([0, 0, link_info[1, 0]]) * Utils.rotx(link_info[2, 0]) * Utils.trn(
         [link_info[3, 0], 0, 0])
     Q02 = Q01 * (Utils.rotz(link_info[0, 0] + theta2) * Utils.trn([0, 0, link_info[1, 1]]) * Utils.rotx(link_info[2, 1]) * Utils.trn(
         [link_info[3, 1], 0, 0]))
@@ -115,120 +95,107 @@ def _create_davinci_arm1(color, opacity):
         [link_info[3, 7], 0, 0]))
     Q09 = Q08 * (Utils.rotz(link_info[0, 7] + theta9) * Utils.trn([0, 0, link_info[1, 8]]) * Utils.rotx(link_info[2, 8]) * Utils.trn(
         [link_info[3, 8], 0, 0]))
-    Q010 = Q09 * (Utils.rotz(link_info[0, 8] + theta10) * Utils.trn([0, 0, link_info[1, 9]]) * Utils.rotx(link_info[2, 9]) * Utils.trn(
+    Q010 = Q09 * (Utils.rotz(link_info[0, 8]) * Utils.trn([0, 0, link_info[1, 9]]) * Utils.rotx(link_info[2, 9]) * Utils.trn(
         [link_info[3, 9], 0, 0]))
 
     link1_mth = Utils.inv_htm(Q01)
-    link_3d_obj.append(
-        [Model3D(
-            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/4.obj', # torre movel
-            scale=scale, htm=link1_mth, mesh_material=mesh),
-            Model3D(
-            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/5.obj', # cilindro conector
-            scale=scale, htm=link1_mth, mesh_material=mesh),
-         ]
-    )
+    link_3d_obj.append([
+        # torre movel
+        Model3D(url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/4.obj',
+                scale=scale, htm=link1_mth, mesh_material=mesh),
+        # cilindro conector
+        Model3D(url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/5.obj',
+                scale=scale, htm=link1_mth, mesh_material=mesh),
+    ])
 
     link2_mth = Utils.inv_htm(Q02)
-    # Part 1
-    link_3d_obj.append(
-        [Model3D(
-            # ligacao de cilindros curta
-            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/8.obj',
-            scale=scale, htm=link2_mth, mesh_material=mesh),
-         ]
-    )
+    link_3d_obj.append([
+        # ligacao de cilindros curta
+        Model3D(url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/8.obj',
+                scale=scale, htm=link2_mth, mesh_material=mesh),
+    ])
 
     link3_mth = Utils.inv_htm(Q03)
-    # Part 2
-    link_3d_obj.append(
-        [Model3D(
-            # cilindro conector
+    link_3d_obj.append([
+        # cilindro conector
+        Model3D(
             url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/59.obj',
             scale=scale, htm=link3_mth, mesh_material=mesh),
-         Model3D(
-            # ligacao de cilindros longa
+        # ligacao de cilindros longa
+        Model3D(
             url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/9.obj',
             scale=scale, htm=link3_mth, mesh_material=mesh),
-         ]
-    )
+    ])
 
     link4_mth = Utils.inv_htm(Q04)
-    # Part 3
-    link_3d_obj.append(
-        [Model3D(
-            # ligacao de cilindros longa
+    link_3d_obj.append([
+        # ligacao de cilindros longa
+        Model3D(
+
             url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/11.obj',
             scale=scale, htm=link4_mth, mesh_material=mesh),
-         ]
-    )
+    ])
 
     link5_mth = Utils.inv_htm(Q05)
-    # Part 4
-    link_3d_obj.append(
-        [Model3D(
-            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/10.obj',  # cotovelo
+    link_3d_obj.append([
+        # cotovelo
+        Model3D(
+            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/10.obj',
             scale=scale, htm=link5_mth, mesh_material=mesh),
-         ]
-    )
+    ])
 
     link6_mth = Utils.inv_htm(Q06)
-    # Part 5
-    link_3d_obj.append(
-        [Model3D(
-            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/38.obj',  # pá dobrada
+    link_3d_obj.append([
+        # pá dobrada
+        Model3D(
+            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/38.obj',
             scale=scale, htm=link6_mth, mesh_material=mesh),
-         ]
-    )
+    ])
 
     link7_mth = Utils.inv_htm(Q07)
-    # Part 6, 7, 8
-    link_3d_obj.append(
-        [Model3D(
-            # conecta pá com bumerangue
+    link_3d_obj.append([
+        # conecta pá com bumerangue
+        Model3D(
             url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/19.obj',
             scale=scale, htm=link7_mth, mesh_material=mesh),
-         ]
-    )
+    ])
 
     link8_mth = Utils.inv_htm(Q08)
-    # Part 9
-    link_3d_obj.append(
-        [
-            Model3D(
-                url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/21.obj',  # bumerangue
-                scale=scale, htm=link8_mth, mesh_material=mesh),
+    link_3d_obj.append([
+        # bumerangue
+        Model3D(
+            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/21.obj',
+            scale=scale, htm=link8_mth, mesh_material=mesh),
 
-        ]
-    )
+    ])
 
     link9_mth = Utils.inv_htm(Q09)
-    # Part 10, 11
-    link_3d_obj.append(
-        [Model3D(
-            # envoltoria agulha
+    link_3d_obj.append([
+        # envoltoria agulha
+        Model3D(
             url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/43.obj',
             scale=scale, htm=link9_mth, mesh_material=mesh),
-         ]
-    )
+    ])
 
     link10_mth = Utils.inv_htm(Q010)
-    link_3d_obj.append(
-        [Model3D(
-            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/45.obj',  # guia
-            scale=scale, htm=link10_mth, mesh_material=mesh),
-         Model3D(
-            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/54.obj',  # agulha
-            scale=scale, htm=link10_mth, mesh_material=mesh),
-         Model3D(
-            # abridor de garrafa
+    link_3d_obj.append([
+        # guia
+        Model3D(
+            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/45.obj',
+                scale=scale, htm=link10_mth, mesh_material=mesh),
+        # agulha
+        Model3D(
+            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/54.obj',
+                scale=scale, htm=link10_mth, mesh_material=mesh),
+        # abridor de garrafa
+        Model3D(
             url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/51.obj',
             scale=scale, htm=link10_mth, mesh_material=mesh),
-         Model3D(
-            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/52.obj',  # botao
-            scale=scale, htm=link10_mth, mesh_material=mesh),
-         ]
-    )
+        # botao
+        Model3D(
+            url='https://raw.githubusercontent.com/fbartelt/uaibot/master/contents/DaVinci3/52.obj',
+                scale=scale, htm=link10_mth, mesh_material=mesh),
+    ])
 
     links = []
     for i in range(n):
@@ -240,8 +207,8 @@ def _create_davinci_arm1(color, opacity):
     # Define initial configuration
     #     1  2  3  4  5  6  7  8  9 10
     q0 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    htm_n_eef = Utils.trn([0, 0, 0.3025])#np.identity(4)
-    htm_base_0 = Utils.trn([-b1, -b2, b3])  # np.identity(4)
+    htm_n_eef = Utils.trn([0, 0, 0.3025]) 
+    htm_base_0 = Utils.trn([-b1, -b2, b3])
 
     # Create joint limits
     # joint_limits = (np.pi / 180) * np.matrix([[-180, 180], [-180, 180], [-180, 180]])
