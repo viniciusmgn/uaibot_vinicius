7584eb61eead9fa09066115afad67e40d51081f3
fbartelt <fbarteltp@gmail.com>
2022-09-30 22:58:35 -0300

    Add some  collision models for arm1

--- uaibot/robot/_create_davinci_arm1.py
+++ uaibot/robot/_create_davinci_arm1.py
@@ -235,24 +235,24 @@ def _create_davinci_arm1(color, opacity, name=''):
     col_model[5].append(Box(htm=Utils.trn([0, -0.15, -0.125]) @ Utils.roty(np.pi/2),
                             name=name + "_C5_1", width=0.05, height=0.09, depth=0.4, color="orange", opacity=0.3))
 
-    col_model[6].append(Box(htm=Utils.trn([-0.2, 0.025, -0.088]),
+    col_model[6].append(Box(htm=Utils.trn([-0.2, 0.025, -0.028]),
                             name=name + "_C6_0", width=0.25, height=0.14, depth=0.15, color="cyan", opacity=0.3))
-    col_model[6].append(Box(htm=Utils.trn([-0.02, 0.025, -0.088]),
+    col_model[6].append(Box(htm=Utils.trn([-0.02, 0.025, -0.09]),
                             name=name + "_C6_1", width=0.14, height=0.02, depth=0.15, color="cyan", opacity=0.3))
-    col_model[6].append(Box(htm=Utils.trn([-0.019, 0.025, 0.019]),
+    col_model[6].append(Box(htm=Utils.trn([-0.019, 0.023, 0.018]),
                             name=name + "_C6_2", width=0.14, height=0.02, depth=0.15, color="cyan", opacity=0.3))
 
-    col_model[7].append(Box(htm=Utils.trn([-0.42, 0.08, -0.034]) @ Utils.rotz(np.pi/3),
+    col_model[7].append(Box(htm=Utils.trn([-0.42, 0.08, -0.0365]) @ Utils.rotz(np.pi/3),
                             name=name + "_C7_0", width=0.23, height=0.082, depth=0.13, color="#88264a", opacity=0.3))
-    col_model[7].append(Box(htm=Utils.trn([-0.22, 0.08, -0.034]) @ Utils.rotz(np.pi*7/18),
+    col_model[7].append(Box(htm=Utils.trn([-0.22, 0.08, -0.0365]) @ Utils.rotz(np.pi*7/18),
                             name=name + "_C7_1", width=0.11, height=0.09, depth=0.39, color="#88264a", opacity=0.3))
-    col_model[7].append(Box(htm=Utils.trn([-0.023, 0.01, 0.005]) @ Utils.rotz(np.pi*7/18),
-                            name=name + "_C7_2", width=0.08, height=0.01, depth=0.03, color="#88264a", opacity=0.3))
-    col_model[7].append(Box(htm=Utils.trn([-0.023, 0.01, -0.068]) @ Utils.rotz(np.pi*7/18),
-                            name=name + "_C7_3", width=0.08, height=0.01, depth=0.03, color="#88264a", opacity=0.3))
-    col_model[7].append(Cylinder(htm=Utils.trn([0, 0, -0.068]),
+    col_model[7].append(Box(htm=Utils.trn([-0.023, 0.01, -0.073]) @ Utils.rotz(np.pi*7/18),
+                            name=name + "_C7_2", width=0.08, height=0.01, depth=0.04, color="#88264a", opacity=0.3))
+    col_model[7].append(Box(htm=Utils.trn([-0.023, 0.01, 0.0]) @ Utils.rotz(np.pi*7/18),
+                            name=name + "_C7_3", width=0.08, height=0.01, depth=0.04, color="#88264a", opacity=0.3))
+    col_model[7].append(Cylinder(htm=Utils.trn([0, 0, 0.0]),
                                  name=name + "_C7_4", radius=0.035, height=0.01, color="#88264a", opacity=0.3))
-    col_model[7].append(Cylinder(htm=Utils.trn([0, 0, 0.005]),
+    col_model[7].append(Cylinder(htm=Utils.trn([0, -0.001, -0.073]),
                                  name=name + "_C7_5", radius=0.035, height=0.01, color="#88264a", opacity=0.3))
 
     col_model[8].append(Box(htm=Utils.trn([-0.052, 0.0, -0.042]),
