e59861105d76b4875203dd9133b119d8dca692cc
fbartelt <fbarteltp@gmail.com>
2022-10-21 16:57:58 -0300

    Change robot initial configuration

--- uaibot/demo/control_test.py
+++ uaibot/demo/control_test.py
@@ -5,7 +5,7 @@ from simobjects.frame import Frame
 from utils import Utils
 import robot as rb
 from cvxopt import matrix, solvers
-from graphics.meshmaterial import MeshMaterial
+from graphics.meshmaterial import MeshMaterial, Texture
 import sys
 import time
 
@@ -18,26 +18,28 @@ def _control_demo_davinci(arm=0):
     arms = [robot_arm1, robot_arm2, robot_arm3, robot_arm4]
     robot = arms[arm]
 
-    # texture_wall = Texture(
-    #     url='https://raw.githubusercontent.com/viniciusmgn/uaibot_vinicius/master/contents/Textures/rough_metal.jpg',
-    #     wrap_s='RepeatWrapping', wrap_t='RepeatWrapping', repeat=[4, 4])
+    texture_wall = Texture(
+        url='https://raw.githubusercontent.com/viniciusmgn/uaibot_content/master/contents/Textures/rough_metal.jpg',
+        wrap_s='RepeatWrapping', wrap_t='RepeatWrapping', repeat=[4, 4])
 
-    material_wall = MeshMaterial(roughness=1, metalness=1)
+    material_wall = MeshMaterial(texture_map=texture_wall, roughness=1, metalness=1)
 
-    wall1 = Box(name="wall1", htm=Utils.trn([1.2e1, 0.5, 0.7]), width=0.05, depth=0.6, height=1.4e-2,
+    wall1 = Box(name="wall1", htm=Utils.trn([0.6 - 0.9, 0.5, 0.8]) @ Utils.roty(-np.pi/2), width=0.05, depth=0.2, height=1.4,
                 mesh_material=material_wall)
-    wall2 = Box(name="wall2", htm=Utils.trn([1.2e1, 1.5, 0.7]), width=0.05, depth=0.6, height=1.4e-2,
+    wall2 = Box(name="wall2", htm=Utils.trn([0.6 - 0.9, 1.1, 0.8]) @ Utils.roty(-np.pi/2), width=0.05, depth=0.2, height=1.4,
                 mesh_material=material_wall)
-    wall3 = Box(name="wall3", htm=Utils.trn([1.2e1, 1, 0.25]), width=0.05, depth=0.4, height=0.5e-2,
+    wall3 = Box(name="wall3", htm=Utils.trn([0. - 0.9, 0.75, 0.8]) @ Utils.roty(-np.pi/2), width=0.05, depth=0.6, height=0.2,
                 mesh_material=material_wall)
-    wall4 = Box(name="wall4", htm=Utils.trn([1.2e1, 1, 1.15]), width=0.05, depth=0.4, height=0.5e-2,
+    wall4 = Box(name="wall4", htm=Utils.trn([0.7 - 0.9, 0.75, 0.8]) @ Utils.roty(-np.pi/2), width=0.05, depth=0.6, height=0.5,
+                mesh_material=material_wall)
+    wall5 = Box(name="wall5", htm=Utils.trn([1.4 - 0.9, 0.8, 0.8]) @ Utils.roty(-np.pi/2), width=0.05, depth=0.8, height=0.2,
                 mesh_material=material_wall)
 
-    def evaluate_error(r):
+    def evaluate_error(r, tol_pos=5e-3):
         error_pos = max(abs(r[0:3]))
-        error_pos = np.linalg.norm(r[0:3])
+        #error_pos = np.linalg.norm(r[0:3])
         error_ori = (180 / np.pi) * max(abs(np.arccos(1 - r[3:6])))
-        ok1 = error_pos < 0.005
+        ok1 = error_pos < tol_pos
         ok2 = error_ori < 5 if len(r.tolist()) > 3 else True
 
         return ok1 and ok2, error_pos, error_ori
@@ -54,43 +56,40 @@ def _control_demo_davinci(arm=0):
 
         struct = [dist_wall_1, dist_wall_2, dist_wall_3, dist_wall_4]
 
-        # gradD_list = np.r_[dist_wall_1.get_closest_item().jac_distance, dist_wall_2.get_closest_item().jac_distance,
-        #                    dist_wall_3.get_closest_item().jac_distance, dist_wall_4.get_closest_item().jac_distance]
         gradD_list = np.block(
             [[dist_wall_1.jac_dist_mat], [dist_wall_2.jac_dist_mat], [dist_wall_3.jac_dist_mat],
              [dist_wall_4.jac_dist_mat]])
         dist_vect = np.block(
             [[dist_wall_1.dist_vect], [dist_wall_2.dist_vect], [dist_wall_3.dist_vect], [dist_wall_4.dist_vect]])
-        # dist_vect = np.block([[dist_wall_1.get_closest_item().distance], [dist_wall_2.get_closest_item().distance], [
-        #                      dist_wall_3.get_closest_item().distance], [dist_wall_4.get_closest_item().distance]])
 
         return dist_vect, gradD_list, struct
 
     # Target pose definition
     pose_tg = []
-    pose_tg.append(Utils.trn([0.3, 1, 1.2]) @ Utils.rotx(-np.pi/2))
-    #pose_tg.append(Utils.trn([0.5, 0, 0.7]) @ Utils.roty(3.14 / 2))
-    #pose_tg.append(robot.fkm(axis="eef"))
+    #pose_tg.append(Utils.trn([0.2, 0.8, 0.7]) @ Utils.rotz(np.deg2rad(-200)) @ Utils.rotx(np.deg2rad(250)))
+    pose_tg.append(Utils.trn([0.2, 0.8, 0.7]) @ Utils.rotx(np.deg2rad(200)) @ Utils.rotz(np.deg2rad(-130))) # rotz -100 ou -130
+    pose_tg.append(Utils.trn([-0.1, 0.8, 0.9]) @ Utils.rotx(np.deg2rad(200)) @ Utils.rotz(np.deg2rad(-130)))
+    pose_tg.append(Utils.trn([-0.5, 0.8, 0.7]) @ Utils.rotx(np.deg2rad(200)) @ Utils.rotz(np.deg2rad(-150)))
 
     # Create simulation
     sim = Simulation.create_sim_grid(
-        [robot, wall1, wall2, wall3, wall4, chest])
+        [robot, wall1, wall2, wall3, wall4, wall5, chest])
 
     for k in range(len(pose_tg)):
         sim.add(Frame(name="pose_tg_" + str(k), htm=pose_tg[k]))
-
+    
     # Parameters
     dt = 0.03
     alpha = 2
     beta = 0.005
-    K = 10
-    gamma = 0.05
+    K = 2
+    eta = 0.9
     sigma = 0.5
     dist_safe = 0.2
     # qdot_max = np.matrix([[2], [np.deg2rad(85)], [np.deg2rad(100)], [np.deg2rad(75)], [np.deg2rad(130)], [np.deg2rad(135)], [np.deg2rad(135)], [np.deg2rad(135)], [np.deg2rad(135)], [2]])
     # qdot_min = -qdot_max
-    xi = 0.05
-    h = 0.05
+    xi = 1
+    h = 0.00002
     q = robot.q.astype(float)
     n = len(robot.links)
     qdot = np.zeros((n, 1)).astype(float)
@@ -98,8 +97,8 @@ def _control_demo_davinci(arm=0):
     # Initializations
     struct = [None, None, None, None]
 
-    i = 0
-    imax = round(47.4/dt)
+    i = 1
+    imax = round((47.4/2)/dt)
 
     hist_dist = []
     hist_time = []
@@ -111,6 +110,14 @@ def _control_demo_davinci(arm=0):
 
     error_qp = False
     iteration_end = False
+
+    # Changing Initial configuration
+    qaux = q.copy()
+    qaux[4] = -np.pi/2
+    qaux[5] = -np.deg2rad(120)
+    qaux[6] = -np.deg2rad(110)
+    qaux[7] = 0
+    robot.add_ani_frame(0, qaux)
 
     # Main loop
     for k in range(len(pose_tg)):
@@ -144,31 +151,26 @@ def _control_demo_davinci(arm=0):
 
             # Create the quadratic program parameters
             A = -gradD_list
-            b = gamma * dist_vect
-            H = 2 * (jac_r.T * jac_r)
+            b = eta * (dist_vect - 0.02)
+            H = 2 * (jac_r.T * jac_r) + 0.01 * np.identity(n)
             f = (2 * K * r.T @ jac_r).T
-            #print(i)
 
-            A = np.block([[A], [np.identity(n)]])
-            A = np.block([[A], [-np.identity(n)]])
+            A = np.block([[A], [np.identity(n)], [-np.identity(n)]])
+            #A = np.block([[A], ])
 
             b = np.block([[b], [xi * qdot_max], [-xi * qdot_min]])
 
             # Solve the quadratic program
-            #print(gradD_list.shape, dist_vect.shape, qdot.shape)
-            #print(H.shape, f.shape, A.shape, b.shape)
-            #qddot = solvers.qp(matrix(H), matrix(f), matrix(-A), matrix(-b))['x']
             try:
-                qddot = solvers.qp(matrix(H), matrix(f), matrix(A), matrix(b))['x']
+                qdot = solvers.qp(matrix(H), matrix(f), matrix(A), matrix(b))['x']
             except:
-                qddot = np.matrix(np.zeros((n, 1)))
+                qdot = np.matrix(np.zeros((n, 1)))
                 error_qp = True
 
-            qddot = np.reshape(qddot, (n, 1))
+            qdot = np.array(qdot).reshape(n, 1)
 
             # First order explicit Euler simulation
             q += qdot * dt
-            qdot += qddot * dt
 
             # Add animation to simulation
             robot.add_ani_frame(i * dt, q)
@@ -181,29 +183,30 @@ def _control_demo_davinci(arm=0):
             hist_r = np.block([hist_r, r])
             hist_q = np.block([hist_q, q])
             hist_qdot = np.block([hist_qdot, qdot])
-            hist_qddot = np.block([hist_qddot, qddot])
-            V = alpha * (np.linalg.norm(r_dot + alpha * r) ** 2) + \
-                sigma * beta * (np.linalg.norm(qdot) ** 2)
-            hist_V.append(V)
 
             # Continue the loop, check if converged
             i += 1
-            converged, error_pos, error_ori = evaluate_error(r)
+            converged, error_pos, error_ori = evaluate_error(r, 5e-4)
+            if converged:
+                print(error_pos, error_ori)
 
             iteration_end  = i>imax
     print(converged, error_qp, iteration_end, i)
-    #print(k, i)
+    # for i in robot.links:
+    #     for j in i.col_objects:
+    #         sim.add(j[0])
+    # robot.update_col_object(0)
     # Run simulation
     sim.run()
 
     # Plot graphs
-    # Utils.plot(hist_time, hist_dist, "", "Time (s)", "True distance (m)", "dist")
-    # Utils.plot(hist_time, hist_q, "", "Time (s)", "Joint configuration (rad)", "q")
-    # Utils.plot(hist_time, hist_qdot, "", "Time (s)", "Joint speed (rad/s)", "qdot")
-    # Utils.plot(hist_time, hist_qddot, "", "Time (s)", "Joint acceleration (rad/s²)", "u")
-    # Utils.plot(hist_time, hist_V, "", "Time (s)", "Lyapunov function", "V")
-    # fig = Utils.plot(hist_time, hist_r, "", "Time (s)", "Task function",
-    #                  ["posx", "posy", "posz", "orix", "oriy", "oriz"])
+    Utils.plot(hist_time, hist_dist, "", "Time (s)", "True distance (m)", "dist")
+    Utils.plot(hist_time, hist_q, "", "Time (s)", "Joint configuration (rad)", "q")
+    Utils.plot(hist_time, hist_qdot, "", "Time (s)", "Joint speed (rad/s)", "qdot")
+    #Utils.plot(hist_time, hist_qddot, "", "Time (s)", "Joint acceleration (rad/s²)", "u")
+    #Utils.plot(hist_time, hist_V, "", "Time (s)", "Lyapunov function", "V")
+    fig = Utils.plot(hist_time, hist_r, "", "Time (s)", "Task function",
+                     ["posx", "posy", "posz", "orix", "oriy", "oriz"])
 
     return sim, (hist_time, hist_dist, hist_r, hist_q, hist_qdot)
 
